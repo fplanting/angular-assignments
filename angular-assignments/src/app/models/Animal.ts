9474814a4788368c784fe336418fdf73e70e475e
fplanting <frida.planting@hotmail.com>
2022-02-09 13:33:18 +0100

    added component and models

---
+++ angular-assignments/src/app/models/Animal.ts
@@ -0,0 +1,13 @@
+export class Animal {
+    type: string;
+    name: string;
+    age: number;
+
+
+    constructor(newAnimal: string, newAge: number) {
+        this.type = newAnimal;
+        this.name = newAnimal;
+        this.age = newAge;
+    }
+
+}
